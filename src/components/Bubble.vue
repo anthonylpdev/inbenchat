e9a97392614e8970d2efdf6aa54bd038595f6513
LOPEZ Anthony (i-BP) <Anthony.LOPEZ@i-bp.fr>
2020-02-14 22:46:33 +0100

    Bubble component setup and style

---
+++ src/components/Bubble.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bubble">{{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Bubble',
+  props: ['message'],
+};
+</script>
+
+<style scoped>
+  .bubble {
+    max-width: 70%;
+    background: linear-gradient(93.27deg, #60A9F6 0%, #2A8BF2 100%);
+    /* Shadow Block Message My Opponent */
+    box-shadow: 10px 10px 50px rgba(42, 139, 242, 0.1),
+    15px 15px 35px rgba(42, 139, 242, 0.05),
+    10px 10px 25px rgba(42, 139, 242, 0.1);
+    border-radius: 0px 10px 10px 10px;
+    color: #fff;
+    font-size: 16px;
+    line-height: 26px;
+    padding: 14px 22px 25px;
+    text-align: left;
+    margin-bottom: 15px;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="head-part"></div>
+    <Bubble
+      v-for="(message, index) in this.messages"
+      :key="index"
+      :message="message"
+    />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import Bubble from '../components/Bubble.vue';
+
+const messages = [
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur debitis, dolores et molestiae nam odit quisquam repudiandae sed sit.',
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur debitis, dolores et molestiae nam odit quisquam repudiandae sed sit.',
+  'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consequatur debitis, dolores et molestiae nam odit quisquam repudiandae sed sit.',
+];
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
+    Bubble,
+  },
+  data() {
+    return {
+      messages,
+    };
   },
 };
 </script>
+
+<style scoped>
+  .home {
+    float: right;
+    height: 80vh;
+    width: 850px;
+    background-color: #fff;
+    padding-left: 50px;
+    padding-top: 14px;
+    margin-right: 40px;
+  }
+</style>
