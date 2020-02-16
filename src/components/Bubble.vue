e95545f4b0391d040c421bd1f4274948f1040c8d
LOPEZ Anthony (i-BP) <Anthony.LOPEZ@i-bp.fr>
2020-02-16 19:51:12 +0100

    Fetch message data from back API

--- src/components/Bubble.vue
+++ src/components/Bubble.vue
@@ -1,5 +1,11 @@
 <template>
-  <div class="bubble" :class="{ me: message.me }">
+  <div class="bubble" :class="{ me: message.user_id === 1 }">
+    <img class="avatar"
+         src="../assets/avatar-male.jpg"
+         width="35"
+         height="35"
+         title="Sam Jr"
+         alt="Sam Jr" />
     <span class="content">{{ message.content }}</span>
   </div>
 </template>
@@ -11,15 +17,30 @@ export default {
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
   .bubble {
     display: flex;
+    flex-direction: row;
     justify-content: flex-start;
     margin-bottom: 20px;
+    &.me {
+      flex-direction: row-reverse;
+      .content {
+        background: linear-gradient(93.27deg, #60A9F6 0%, #2A8BF2 100%);
+        box-shadow: 10px 10px 50px rgba(42, 139, 242, 0.1),
+        15px 15px 35px rgba(42, 139, 242, 0.05),
+        10px 10px 25px rgba(42, 139, 242, 0.1);
+        border-radius: 10px 10px 0 10px;
+        color: #fff;
+        margin-left: 0;
+        margin-right: 24px;
+      }
+    }
   }
 
   .content {
     max-width: 70%;
+    margin-left: 24px;
     color: #707C97;
     background: #FFFFFF;
     border: 1px solid rgba(112, 124, 151, 0.25);
@@ -28,22 +49,12 @@ export default {
     10px 10px 25px rgba(112, 124, 151, 0.05);
     font-size: 16px;
     line-height: 26px;
-    padding: 14px 22px 25px;
+    padding: 14px 22px;
     text-align: left;
     border-radius: 0px 10px 10px 10px;
   }
 
-  .me {
-    justify-content: flex-end;
-  }
-
-  .bubble.me .content {
-    background: linear-gradient(93.27deg, #60A9F6 0%, #2A8BF2 100%);
-    /* Shadow Block Message My Opponent */
-    box-shadow: 10px 10px 50px rgba(42, 139, 242, 0.1),
-    15px 15px 35px rgba(42, 139, 242, 0.05),
-    10px 10px 25px rgba(42, 139, 242, 0.1);
-    border-radius: 10px 10px 0 10px;
-    color: #fff;
+  .avatar {
+    border-radius: 50%;
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,34 @@
 <template>
   <div class="home">
-    <div class="head-part"></div>
-    <Bubble
-      v-for="(message, index) in this.messages"
-      :key="index"
-      :message="message"
-    />
+    <div class="inside head-part">
+      <img class="avatar"
+           src="../assets/avatar-male.jpg"
+           width="54"
+           height="54"
+           title="Sam Jr"
+           alt="Sam Jr"/>
+      <span class="name">Sam Jr</span>
+    </div>
+    <div class="inside body-part">
+      <Bubble
+        v-for="(message, index) in this.messages"
+        :key="index"
+        :message="message"
+      />
+    </div>
+    <div class="inside footer-part">
+      <form class="txt" action="">
+        <input type="text" placeholder="Ecrire un message...">
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
 import Bubble from '../components/Bubble.vue';
 
+/*
 const messages = [
   {
     me: true,
@@ -42,6 +59,8 @@ const messages = [
     content: 'Super ! Soit le bienvenue Sam !',
   },
 ];
+*/
+const messages = [];
 
 export default {
   name: 'Home',
@@ -53,14 +72,68 @@ export default {
       messages,
     };
   },
+  mounted() {
+    axios.get('http://localhost:3030/message')
+      .then((response) => {
+        this.messages = response.data.data;
+      });
+  },
 };
+
+
 </script>
 
-<style scoped>
+<style scoped lang="scss">
   .home {
-    height: 80vh;
     width: 850px;
     background-color: #fff;
-    padding: 80px;
+    .inside {
+      padding: 0 60px;
+    }
+    .head-part {
+      display: flex;
+      flex-direction: column;
+      height: 140px;
+      background: #FAFBFF;
+      border-radius: 6px 6px 0px 0px;
+      justify-content: center;
+      align-items: center;
+      text-align: center;
+      .avatar {
+        display: block;
+        border-radius: 50%;
+        margin-bottom: 2px;
+      }
+      .name {
+        font-weight: bold;
+      }
+    }
+    .body-part {
+      height: 500px;
+      overflow-y: scroll;
+      padding-top: 40px;
+      padding-bottom: 40px;
+    }
+    .footer-part {
+      .txt {
+        height: 98px;
+        display: block;
+        width: 100%;
+        border-top: 2px solid #f2f2f2;
+        input {
+          display: block;
+          width: 100%;
+          border: none;
+          font-family: Avenir, Helvetica, Arial, sans-serif;
+          padding: 27px 20px;
+          text-align: left;
+          font-size: 20px;
+          line-height: 24px;
+          &:focus {
+            outline: none;
+          }
+        }
+      }
+    }
   }
 </style>
