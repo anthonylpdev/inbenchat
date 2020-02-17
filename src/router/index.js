38ed15fb48e9a3ca415f5f3c279f9838e78bcb32
LOPEZ Anthony (i-BP) <Anthony.LOPEZ@i-bp.fr>
2020-02-17 16:53:47 +0100

    Login component and authentification

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,32 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Login from '../views/Login.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
+    path: '/login',
+    name: 'Login',
+    component: Login,
+  },
+  {
     path: '/',
     name: 'Home',
     component: Home,
+    beforeEnter: (to, from, next) => {
+      if (localStorage.getItem('user-token') !== null) {
+        next();
+      } else {
+        next({
+          name: 'Login',
+        });
+      }
+    },
   },
+  { path: '*', redirect: '/' },
 ];
-
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
